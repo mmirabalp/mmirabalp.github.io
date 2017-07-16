f5cedbdd6cced67e3e62ce2aaf704170ad02a27c
Mawel Mirabal <mmirabalp@gmail.com>
2017-07-15 19:57:41 -0700

    update route

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module("myApp", ["ngRoute"]);
 
 app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl : "views/home.htm"
         }).
-        when("/home", {
-            templateUrl : "views/home.htm"
-        }).
+
         when("/project", {
             templateURL : "views/project.htm"
-        }).
-        otherwise("/");
+        });
 
 });
