3dc7bccdafe622461884ca57797744a63ca67c3a
Mawel Mirabal <mmirabalp@gmail.com>
2017-07-28 18:24:10 -0700

    Passing $scope.title to Home.html Work In Progress

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ app.config(function($routeProvider) {
             templateUrl: "views/home.htm",
             controller: "homeCtrl"
         })
-        .when("/project/:id", {
+        .when("/project/:title", {
             templateUrl: "views/project.htm",
             controller: "projectCtrl"
         })
@@ -14,21 +14,169 @@ app.config(function($routeProvider) {
 
 });
 
+app.factory('dataAccess', function($http){
+    return {
+        projects : $http.get('js/data.json').then(function(response) {
+            return response;
 
-// Json data projects connection
-app.controller('homeCtrl', function($scope, $http) {
+        })
 
-    $http.get('js/data.json').then(function (response) {
-        $scope.projects = response.data;
+
+
+    };
+});
+
+
+
+
+
+
+// app.factory('dataAccess', ['$http', function($http) {
+//
+//     var urlBase = 'js/data.json';
+//     var dataFactory = {
+//         datos:  $http.get(urlBase)
+//     }
+//
+//
+//         return dataFactory;
+//
+//
+// }]);
+
+
+////Working Factory
+//app.factory("dataAccess", ['$http', "$scope",function($http, $scope){
+//
+//    var proj = {};
+//
+//    proj.fetchUserDetails = function(){   /*This just to demonstrate common usage of factories to serve $http responses, by returning the entire promise */
+//        return $http.get('js/data.json');
+//    };
+//
+//
+//    return proj;
+//
+//
+//
+//
+//
+//}]);
+
+
+
+
+
+app.factory("dataAccess", function($http){
+//    var factObj = {
+//
+//        infoData: { },
+//        getInfo: function(){
+//             $http.get("js/data.json").then(function(info){
+////                 factObj = info.$$state.value.baseUrl;
+//
+//
+//
+//            });
+//
+//        }
+
+  return{
+      infoData : function (){
+          return $http.get('js/data.json').then(function(response){
+              return response.data;
+          });
+      }
+
+  };
+
+
+
+
+});
+
+
+
+app.controller('homeCtrl', function($scope, dataAccess){
+    console.log(dataAccess);
+
+
+    dataAccess.infoData().then(function(data){
+        $scope.projects = data;
+
     });
 
+
 });
+
+
+app.controller('projectCtrl', function($scope, dataAccess){
+    console.log(dataAccess);
+
+
+    dataAccess.infoData().then(function(data){
+        $scope.projects = data;
+
+    });
+
+
+});
+
+
+
+
+
 
 // Json data projects connection
-app.controller('projectCtrl', function($scope, $routeParams) {
+//app.controller('homeCtrl', function($scope, $http, dataAccess)
+//{
+////    console.log(dataAccess.$$state);
+////
+////    console.log(dataAccess.projects);
+////
+////    console.log(dataAccess);
+//
+//
+//
+//
+////    $http.get('js/data.json').then(function (response) {
+////        $scope.projects = response.data;
+//
+//
+//
+//    dataAccess.getUser().then(function(user){
+//        dataAccess.user = user;
+//        console.log(dataAccess);
+//    });
+//
+////
+//    console.log(dataAccess.projects.$$state.status);
+////    });
+//
+//
+//
+//});
 
-    console.log( $routeParams.id );
-    console.log( $scope.title );
 
-});
+
+
+
+//// Json data projects connection
+//app.controller('projectCtrl', function($scope, $routeParams, dataAccess) {
+//
+//    // console.log($routeParams);
+//    // console.log($scope.projects);
+//
+//
+//
+//    // dataAccess.fetchUserDetails().then(function(response){ /*This is an example of how you can process and return the required result (Notice here we use '.then')*/
+//    //     $scope.project = response.data;
+//        // console.log(response.data[0].title);
+//        // console.log($scope.projects[0].title);
+//        // console.log($scope.project);
+//        // console.log($routeParams);
+//    // });
+//
+//
+//});
 
