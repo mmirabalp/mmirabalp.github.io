9ed06e7a46f4b4a863fcda567e3cbb7dc4188e8c
Mawel Mirabal <mmirabalp@gmail.com>
2017-07-28 23:02:55 -0700

    Finish Share Data Between Controllers and views
    Finish Share data between Project Controllers Using a Factory, and show
    details about single project in the project page.

--- js/main.js
+++ js/main.js
@@ -14,80 +14,10 @@ app.config(function($routeProvider) {
 
 });
 
-//app.factory('dataAccess', function($http){
-//    return {
-//        projects : $http.get('js/data.json').then(function(response) {
-//            return response;
-//
-//        })
-//
-//
-//
-//    };
-//});
-
-
-
-
-
-
-// app.factory('dataAccess', ['$http', function($http) {
-//
-//     var urlBase = 'js/data.json';
-//     var dataFactory = {
-//         datos:  $http.get(urlBase)
-//     }
-//
-//
-//         return dataFactory;
-//
-//
-// }]);
-
-
-////Working Factory
-//app.factory("dataAccess", ['$http', "$scope",function($http, $scope){
-//
-//    var proj = {};
-//
-//    proj.fetchUserDetails = function(){   /*This just to demonstrate common usage of factories to serve $http responses, by returning the entire promise */
-//        return $http.get('js/data.json');
-//    };
-//
-//
-//    return proj;
-//
-//
-//
-//
-//
-//}]);
-
-
 
 
 
 app.factory("dataAccess", function($http){
-//    var factObj = {
-//
-//        infoData: { },
-//        getInfo: function(){
-//             $http.get("js/data.json").then(function(info){
-////                 factObj = info.$$state.value.baseUrl;
-//
-//
-//
-//            });
-//
-//        }
-
-
-//var deferredObj = new deferred();
-//    var info = null;
-//    deferredObj.resolve(info);
-//
-//    return deferredObj.promise;
-
 
   return{
       infoData : function (){
@@ -101,111 +31,26 @@ app.factory("dataAccess", function($http){
 
 
 
+
 app.controller('homeCtrl', function($scope, dataAccess, $q){
 
-    $q.all([dataAccess]).then(function(data){
-//        console.log(data[0]);
+    $q.all([dataAccess]).then(function(){
+        dataAccess.infoData().then(function(info){
+            $scope.projects = info;
+        });
     });
-
-    dataAccess.infoData().then(function(info){
-        $scope.projects = info;
-        console.log($scope.projects[0].title);
-    });
-
-
-
-
-
-//        dataAccess.infoData().then(function(dataAccess){
-//            $scope.projects = dataAccess;
-//
-//        return $scope.projects;
-//
-//            console.log($scope.projects);
-//
-//    });
-//    console.log(dataAccess.infoData());
-
-
-});
-
-
-app.controller('projectCtrl', function($scope, dataAccess){
-
-    dataAccess.infoData().then(function(dataAccess){
-        $scope.projects = dataAccess;
-        return $scope.projects;
-
-        console.log();
-
-    });
-//    console.log(dataAccess);
-
-
-
-
-//    dataAccess.infoData().then(function(data){
-//        $scope.projects = data;
-//
-//    });
-
 });
 
 
 
 
+app.controller('projectCtrl', function($routeParams, $scope, dataAccess, $q){
+    var id = $routeParams.id - 1;
 
-
-// Json data projects connection
-//app.controller('homeCtrl', function($scope, $http, dataAccess)
-//{
-////    console.log(dataAccess.$$state);
-////
-////    console.log(dataAccess.projects);
-////
-////    console.log(dataAccess);
-//
-//
-//
-//
-////    $http.get('js/data.json').then(function (response) {
-////        $scope.projects = response.data;
-//
-//
-//
-//    dataAccess.getUser().then(function(user){
-//        dataAccess.user = user;
-//        console.log(dataAccess);
-//    });
-//
-////
-//    console.log(dataAccess.projects.$$state.status);
-////    });
-//
-//
-//
-//});
-
-
-
-
-
-//// Json data projects connection
-//app.controller('projectCtrl', function($scope, $routeParams, dataAccess) {
-//
-//    // console.log($routeParams);
-//    // console.log($scope.projects);
-//
-//
-//
-//    // dataAccess.fetchUserDetails().then(function(response){ /*This is an example of how you can process and return the required result (Notice here we use '.then')*/
-//    //     $scope.project = response.data;
-//        // console.log(response.data[0].title);
-//        // console.log($scope.projects[0].title);
-//        // console.log($scope.project);
-//        // console.log($routeParams);
-//    // });
-//
-//
-//});
-
+    $q.all([dataAccess]).then(function(){
+        dataAccess.infoData().then(function(info){
+            $scope.project = info[id];
+            return $scope.project;
+        });
+    });
+});
