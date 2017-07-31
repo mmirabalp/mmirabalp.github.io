13a5fdcb032dc6248cc5e008b875cdcdf9e50c0e
Mawel Mirabal <mmirabalp@gmail.com>
2017-07-31 00:21:51 -0700

    Added Comments to the Project and Some Colors, and some place holders for projects

--- js/main.js
+++ js/main.js
@@ -1,28 +1,34 @@
+//DECLARING ANGULAR MODULE AND app Name: 'myApp',
+//Injecting 'ngRoute' dependency module to use for routes in app.
 var app = angular.module("myApp", ["ngRoute"]);
 
+
+//Configuring the route of the application.
+//$routeProvider is a directive inside ngRoute module
 app.config(function($routeProvider) {
     $routeProvider
-        .when("/", {
-            templateUrl: "views/home.htm",
-            controller: "homeCtrl"
+        .when("/", {   //(when) is a method inside $routeProvider directive
+            templateUrl: "views/home.htm",   //templateUrl contain the browser route as it is there...
+        controller: "homeCtrl"   //Declaring de controller that is going to control this view: "views/home.htm" when the user go to that route in browser
         })
-        .when("/project/:id", {
+        .when("/project/:id", { //'_______'/:id . is a variable value that  $routeParams is going to get.
             templateUrl: "views/project.htm",
         controller: "projectCtrl"
         })
-        .otherwise("/");
+        .otherwise("/");   //Default route that goes to HOME.
 
 });
 
 
 
 
-app.factory("dataAccess", function($http){
+//DECLARING A SERVICE 'Factory' to share data between controller and views
+app.factory("dataAccess", function($http){   //dataAccess is the name of the Factory and $http is a directive that is being injecting for get Json.
 
-  return{
-      infoData : function (){
-          return $http.get('js/data.json').then(function(response){
-              return response.data;
+  return{    //Factory return an object
+      infoData : function (){  //Inside the object, declare a function to return the call to Json file, data will come back in a 'Promise'...
+          return $http.get('js/data.json').then(function(response){  //response is a variable that will hold the promise, it has a value 'data'.
+              return response.data; //return response.data if the call to Json file is success.
           });
       }
   };
@@ -31,12 +37,12 @@ app.factory("dataAccess", function($http){
 
 
 
-
+//DECLARING 'homeCtrl' TO CONTROL views/project.htm,Injecting $scope, dataAccess (The service Factory) and $q for Deferred implementations
 app.controller('homeCtrl', function($scope, dataAccess, $q){
-
-    $q.all([dataAccess]).then(function(){
+    //$q.all access ALL the promise date coming from the factory ([dataAccess]).
+    $q.all([dataAccess]).then(function(){ //If it is success accessing the data promise then it call a function with parameter 'info' to inject the data and assign it to $scope.projects to be use in the project.
         dataAccess.infoData().then(function(info){
-            $scope.projects = info;
+            $scope.projects = info; // Assigning the data to $scope.projects.
         });
     });
 });
@@ -45,12 +51,13 @@ app.controller('homeCtrl', function($scope, dataAccess, $q){
 
 
 app.controller('projectCtrl', function($routeParams, $scope, dataAccess, $q){
-    var id = $routeParams.id - 1;
+    var id = $routeParams.id - 1; //Asign $routeParams content that is = project id - 1.
 
+    // perform some asynchronous operation, resolve or reject the promise when appropriate.
     $q.all([dataAccess]).then(function(){
         dataAccess.infoData().then(function(info){
-            $scope.project = info[id];
-            return $scope.project;
+            $scope.project = info[id]; //info[id] is bringing the data from the single project that has been click it on.
+            return $scope.project; //Return the single project data.
         });
     });
 });
