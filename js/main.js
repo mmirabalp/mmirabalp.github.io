336053397e5e85542ed15ad9d6205b6fee1d0913
Mawel Mirabal <mmirabalp@gmail.com>
2017-08-04 21:58:41 -0700

    Finish Connection to API and Clean up the code...

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 //Injecting 'ngRoute' dependency module to use for routes in app.
 var app = angular.module("myApp", ["ngRoute"]);
 
-
 //Configuring the route of the application.
 //$routeProvider is a directive inside ngRoute module
 app.config(function($routeProvider) {
@@ -24,14 +23,13 @@ app.config(function($routeProvider) {
 
 //DECLARING A SERVICE 'Factory' to share data between controller and views
 app.factory("dataAccess", function($http){   //dataAccess is the name of the Factory and $http is a directive that is being injecting for get Json.
-
-  return{    //Factory return an object
-      infoData : function (){  //Inside the object, declare a function to return the call to Json file, data will come back in a 'Promise'...
-          return $http.get('js/data.json').then(function(response){  //response is a variable that will hold the promise, it has a value 'data'.
+   return{    //Factory return an object
+       apiData : function (){  //Inside the object, declare a function to return the call to Json file, data will come back in a 'Promise'...
+          return $http.get('https://cdn.contentful.com/spaces/pvdxttzvgb3d/entries?access_token=24069431a8de8798be8e2e778db2ab42ed42942b6dfca4bc3e5523bee336d1c7').then(function(response){  //response is a variable that will hold the promise, it has a value 'data'.
               return response.data; //return response.data if the call to Json file is success.
           });
-      }
-  };
+        }
+    };
 
 });
 
@@ -41,23 +39,34 @@ app.factory("dataAccess", function($http){   //dataAccess is the name of the Fac
 app.controller('homeCtrl', function($scope, dataAccess, $q){
     //$q.all access ALL the promise date coming from the factory ([dataAccess]).
     $q.all([dataAccess]).then(function(){ //If it is success accessing the data promise then it call a function with parameter 'info' to inject the data and assign it to $scope.projects to be use in the project.
-        dataAccess.infoData().then(function(info){
-            $scope.projects = info; // Assigning the data to $scope.projects.
+        dataAccess.apiData().then(function(info){
+            $scope.projects = info.items; // Assigning the data to $scope.projects.
+            $scope.image = info.includes; //Assigning to $scope.image
+            return info.items;
         });
     });
+
 });
 
 
 
 
 app.controller('projectCtrl', function($routeParams, $scope, dataAccess, $q){
-    var id = $routeParams.id - 1; //Asign $routeParams content that is = project id - 1.
 
+    console.log($routeParams);
+    var id = $routeParams.id - 1; //Asign $routeParams content that is = project id - 1.
     // perform some asynchronous operation, resolve or reject the promise when appropriate.
     $q.all([dataAccess]).then(function(){
-        dataAccess.infoData().then(function(info){
-            $scope.project = info[id]; //info[id] is bringing the data from the single project that has been click it on.
-            return $scope.project; //Return the single project data.
+
+        dataAccess.apiData().then(function(info){
+
+            $scope.project = info.items[id]; //info[id] is bringing the data from the single project that has been click it on.
+            $scope.project.image = info.includes.Asset[id].fields.file.url;
+            $scope.project.title = info.items[id].fields.title;
+            $scope.project.description = info.items[id].fields.description;
+            $scope.project.technologies = info.items[id].fields.technologies;
+            $scope.project.url = info.items[id].fields.url;
+
         });
     });
 });
