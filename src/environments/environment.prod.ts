f593af314fef799aa577b30e7a9a24061abd4bcd
Mawel Mirabal <mmirabalp@gmail.com>
2018-09-05 00:19:38 -0700

    Added a Service to Production Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+
+  contentful: {
+    spaceId:'pvdxttzvgb3d',
+    token:'e88ac34324a5256d85ce142fe40ed93cec2aebdf6709ce2bc0c5e84c028a779b'
+  }
 };
