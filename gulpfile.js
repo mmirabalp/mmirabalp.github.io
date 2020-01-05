08fb1416b609e4a8f71254628ceda8a764c61261
Mawel Mirabal <mmirabalp@gmail.com>
2020-01-04 18:03:12 -0800

    fixing up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -16,20 +16,13 @@ var gulp  = require('gulp'),
 
 // gulp sass | compiles sass to destination folder
 gulp.task('sass', function() {
-<<<<<<< HEAD
     gulp.src('scss/main.scss')
         .pipe(sass())
         .pipe(gulp.dest('css'));
-=======
-    gulp.src('src/scss/main.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('src/assets/css'));
->>>>>>> 1c869c61ce035046e6e812210a29cbb64416cd5d
 });
 
 // gulp watch | runs browser sync
 gulp.task('watch', function() {
-<<<<<<< HEAD
     browserSync.init({
         server: './'
     });
@@ -37,15 +30,6 @@ gulp.task('watch', function() {
     // watches for changes on scss and html files
     gulp.watch('scss/*.scss', ['sass']);
     gulp.watch(['scss/*.scss', './*.html', './*.htm']).on('change', reload);
-=======
-    // browserSync.init({
-    //     server: './'
-    // });
-
-    // watches for changes on scss and html files
-    gulp.watch('src/scss/*.scss', ['sass']);
-    gulp.watch(['src/scss/*.scss', './*.html', './*.htm']).on('change', reload);
->>>>>>> 1c869c61ce035046e6e812210a29cbb64416cd5d
 });
 
 // gulp | default runs sass and watch
