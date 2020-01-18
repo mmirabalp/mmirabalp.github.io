1086a5dc65f4ae9b511b8826ab50961c7ce6d5ff
Mawel Mirabal <mmirabalp@gmail.com>
2020-01-18 14:41:10 -0800

    changed the API call logic to handle severals API calls

--- src/js/main.js
+++ src/js/main.js
@@ -1,153 +1,115 @@
     //DECLARING ANGULAR MODULE AND app Name: 'myApp',
     //Injecting 'ngRoute' dependency module to use for routes in app.
-    var app = angular.module("myApp", ["ngRoute"]);
+      var app = angular.module("myApp", ["ngRoute"]);
     // var code = angular.module("myCode", ["ngRoute"]);
+  
+    
 
     //Configuring the route of the application.
     //$routeProvider is a directive inside ngRoute module
     app.config(function($routeProvider) {
         $routeProvider
-          .when("/", {
-            //(when) is a method inside $routeProvider directive
-            templateUrl: "/views/home.htm", //templateUrl contain the browser route as it is there...
-            controller: "homeCtrl" //Declaring de controller that is going to control this view: "views/home.htm" when the user go to that route in browser
+
+            .when("/", {
+            templateUrl: "/views/home.htm",
+            controller: "HomeCtrl"
           })
-          .when("/project/:id", {
-            //'_______'/:id . is a variable value that  $routeParams is going to get.
+            .when("/project/:id", {
+            ////'_______'/:id . is a variable value that  $routeParams is going to get.
             templateUrl: "/views/project.htm",
-            controller: "projectCtrl"
+            controller: "ProjectCtrl"
           })
-          .when("/style", {
-            //'_______'/:id . is a variable value that  $routeParams is going to get.
-            templateUrl: "views/style.htm"
-          })
-
-          .otherwise("/");   //Default route that goes to HOME.
-
-    });
-
-
-
-
-
-    // FACTORY SECTION TO USE CONTENTFULL CMS
-    //DECLARING A SERVICE 'Factory' GET PROJECT DATA FROM CONTENTFULL CMS and share data between controller and views
-    app.factory("dataAccess", function($http){   //dataAccess is the name of the Factory and $http is a directive that is being injecting for get Json.
-    return{    //Factory return an object
-        apiData : function (){  //Inside the object, declare a function to return the call to Json file, data will come back in a 'Promise'...
-            return $http.get('https://cdn.contentful.com/spaces/pvdxttzvgb3d/entries?access_token=24069431a8de8798be8e2e778db2ab42ed42942b6dfca4bc3e5523bee336d1c7').then(function(response){  //response is a variable that will hold the promise, it has a value 'data'.
-                return response.data; //return response.data if the call to Json file is success.
-            });
-            }
-        };
-    });
-
-
-    // PROJECTS MODULE SECTION
-    //DECLARING 'homeCtrl' TO CONTROL views/project.htm,Injecting $scope, dataAccess (The service Factory) and $q for Deferred implementations
-    app.controller('homeCtrl', function($scope, dataAccess, $q){
-        //$q.all access ALL the promise date coming from the factory ([dataAccess]).
-        $q.all([dataAccess]).then(function(){ //If it is success accessing the data promise then it call a function with parameter 'info' to inject the data and assign it to $scope.projects to be use in the project.
-            dataAccess.apiData().then(function(info){
-                $scope.projects = info.items; // Assigning the data to $scope.projects.
-                $scope.image = info.includes; //Assigning to $scope.image
-
-                // function url(){
-                //     return image.Asset[$index].fields.file.url;
-                // }    
-                return info.items;
-            });
-        });
-    });
-
-    app.controller('projectCtrl', function($routeParams, $scope, dataAccess, $q){
-        var id = $routeParams.id - 1; //Asign $routeParams content that is = project id - 1.
-        // perform some asynchronous operation, resolve or reject the promise when appropriate.
-        $q.all([dataAccess]).then(function(){
-            dataAccess.apiData().then(function(info){
-                $scope.project = info.items[id]; //info[id] is bringing the data from the single project that has been click it on.
-                $scope.project.image = info.includes.Asset[id].fields.file.url;
-                $scope.project.title = info.items[id].fields.title;
-                $scope.project.description = info.items[id].fields.description;
-                $scope.project.technologies = info.items[id].fields.technologies;
-                $scope.project.url = info.items[id].fields.url;
-                
-            });
-        });
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- //Configuring the route of the application.
-    $routeProvider is a directive inside ngRoute module
-    code.config(function($routeProvider) {
-        $routeProvider
-          .when("/", {
-            //'_______'/:id . is a variable value that  $routeParams is going to get.
-            templateUrl: "/views/codechallenge.htm",
-            controller: "codeCtrl"
-          })
-
-        //   .when("/codechallenge/:id", {
-        //     //'_______'/:id . is a variable value that  $routeParams is going to get.
-        //     templateUrl: "/views/codechallenge.htm",
-        //     controller: "codeCtrl"
+        //   .when("/", {
+        //     ////(when) is a method inside $routeProvider directive
+        //     templateUrl: "/views/home.htm", //templateUrl contain the browser route as it is there...
+        //     controller: "homeCtrl" //Declaring de controller that is going to control this view: "views/home.htm" when the user go to that route in browser
         //   })
-
+        //     .when("/project/:id", {
+        //     ////'_______'/:id . is a variable value that  $routeParams is going to get.
+        //     templateUrl: "/views/project.htm",
+        //     controller: "projectCtrl"
+        //   })
+        //   .when("/style", {
+        //     ////'_______'/:id . is a variable value that  $routeParams is going to get.
+        //     templateUrl: "views/style.htm"
+        //   })
           .otherwise("/");   //Default route that goes to HOME.
 
     });
 
 
-        // FACTORY SECTION TO USE CONTENTFULL CMS
-    //DECLARING A SERVICE 'Factory' to GET CODE DATA FROM CONTENTFULL CMS and share data between controller and views
-    code.factory("codeAccess", function($http){   //dataAccess is the name of the Factory and $http is a directive that is being injecting for get Json.
-    return{    //Factory return an object
-        apiData : function (){  //Inside the object, declare a function to return the call to Json file, data will come back in a 'Promise'...
-            return $http.get('https://cdn.contentful.com/spaces/4tlvg4vrs2ff/entries?access_token=YzJlKvEoD0liw_0aqVg8RNLjnYasOWcwLilxnu2jsHU').then(function(response){  //response is a variable that will hold the promise, it has a value 'data'.
-                return response.data; //return response.data if the call to Json file is success.
+// *********FACTORYS*************FACTORYS*************FACTORYS************FACTORYS************ FACTORYS 
+ // FACTORY TO CALL CONTENTFUL API TO GET {CODES} CHALLENGES   
+app.factory("projectsData",["$http",
+    function(netProtocol){
+      var factoryObject = {
+         getData: function(){
+            return netProtocol.get("https://cdn.contentful.com/spaces/pvdxttzvgb3d/entries?access_token=24069431a8de8798be8e2e778db2ab42ed42942b6dfca4bc3e5523bee336d1c7").then(function(response){
+                factoryObject.data = response.data;
             });
-            }
-        };
-    });
+        }    
+    };
+    factoryObject.getData();
+    return factoryObject;
+  }
+]);
+
+// FACTORY TO CALL CONTENTFUL API TO GET {CODES} CHALLENGES
+app.factory("codesData",["$http",
+    function(netProtocol){
+      var factoryObject = {
+         getData: function(){
+            return netProtocol.get("https://cdn.contentful.com/spaces/4tlvg4vrs2ff/entries?access_token=YzJlKvEoD0liw_0aqVg8RNLjnYasOWcwLilxnu2jsHU").then(function(response){
+                factoryObject.data = response.data;
+            });
+        }    
+    };
+    factoryObject.getData();
+    return factoryObject;
+  }
+]);
+// *********FACTORYS*************FACTORYS*************FACTORYS************FACTORYS************ FACTORYS 
 
 
-    code.controller("codeCtrl", function($routeParams, $scope, codeAccess, $q) {
-      var id = $routeParams.id - 1; //Asign $routeParams content that is = project id - 1.
-      // perform some asynchronous operation, resolve or reject the promise when appropriate.
-      $q.all([codeAccess]).then(function() {
-        codeAccess.apiData().then(function(info) {
-                $scope.code = info.items[0].fields.projectName; //info[id] is bringing the data from the single project that has been click it on.
-                // $scope.project.image = info.includes.Asset[id].fields.file.url;
-                // $scope.project.title = info.items[id].fields.title;
-                // $scope.project.description = info.items[id].fields.description;
-                // $scope.project.technologies = info.items[id].fields.technologies;
-                // $scope.project.url = info.items[id].fields.url;
-          console.log(info.items[0].fields.projectName);
-          
-        });
-      });
-    });
+
+
+// *********CONTROLLER*************CONTROLLER*************CONTROLLER************CONTROLLER************ CONTROLLER
+
+// HOME CONTROLLER TO CONTROLE HOME PAGE AND CONNECT TO API CALL
+app.controller("HomeCtrl",["$scope","projectsData", "codesData",
+    function(scope, projectData, codesData){
+        scope.projects = projectData;
+        console.log(projects);
+        }
+    ]
+);
+// HOME CONTROLLER TO CONTROLE HOME PAGE AND CONNECT TO API CALL
+
+
+
+// PROJECT CONTROLLER TO CONTROLE HOME PAGE AND CONNECT TO API CALL
+app.controller("ProjectCtrl",["$routeParams", "$scope","projectsData", "$q",
+    function(routeParams,scope,projectsData,$q){
+    var id = routeParams.id - 1;
+        scope.projects = projectsData;
+            $q.all([projectsData]).then(function(){
+              projectsData.getData().then(function(){
+                console.log(projectsData.data.items[id]); 
+                    scope.project = projectsData.data.items[id]; //info[id] is bringing the data from the single project that has been click it on.
+                    // scope.project.image = info.includes.Asset[id].fields.file.url;
+                    scope.project.title = projectsData.data.items[id].fields.title;
+                    scope.project.description = projectsData.data.items[id].fields.description;
+                    scope.project.technologies = projectsData.data.items[id].fields.technologies;
+                    scope.project.url = projectsData.data.items[id].fields.url;                    
+              });        
+            });        
+        }    
+    ]
+);
+// PROJECT CONTROLLER TO CONTROLE HOME PAGE AND CONNECT TO API CALL
+
+
+// *********CONTROLLER*************CONTROLLER*************CONTROLLER************CONTROLLER************ CONTROLLER  
 
 
 
@@ -160,11 +122,3 @@
 
 // Code Challenge Contentfull API Link 
 //(https://cdn.contentful.com/spaces/4tlvg4vrs2ff/entries?access_token=YzJlKvEoD0liw_0aqVg8RNLjnYasOWcwLilxnu2jsHU)
-
-
-
-
-
-
-
-
